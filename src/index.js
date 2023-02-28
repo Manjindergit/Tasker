e2171086b678a9ff0ade46a529e95b7b2a510a1e
Manjindergit <manjindergrewal1997@gmail.com>
2023-02-28 06:02:52 -0800

    css loader working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 console.log('objsddhellosfect');
+import './style.css';
+import { contactPage } from "./dom.js";
+
+document.body.appendChild(contactPage());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
     mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
    plugins: [
      new HtmlWebpackPlugin({
        title: 'Development',
@@ -13,5 +17,15 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean:true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  
 };
